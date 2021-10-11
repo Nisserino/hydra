424d92871db2b8be882f4ff36335e7a1fc8b3ef6
phillodelphia <philip.vallentuna@gmail.com>
2021-10-11 21:35:23 +0200

    saving before destruction

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,13 +17,6 @@ provider "openstack" {
   #region             = "" # use $OS_REGION_NAME
 }
 
-resource "openstack_compute_instance_v2" "server" {
-  for_each            = toset(var.instances_name)
-  name                = each.key
-  image_name          = var.image_name
-  flavor_name         = var.flavor_name
-}
-
 resource "openstack_networking_network_v2" "network" {
 for_each = var.networks
 name = each.key
@@ -38,3 +31,189 @@ ip_version      = 4
 dns_nameservers = var.dns_ip
 }
 
+resource "openstack_compute_secgroup_v2" "secgroup_1" {
+  name                = "TerraformSG"
+  description         = "Terraform SG for Port 80 and 443"
+
+  rule {
+    from_port         = 80
+    to_port           = 80
+    ip_protocol       = "tcp"
+    cidr              = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port         = 443
+    to_port           = 443
+    ip_protocol       = "tcp"
+    cidr              = "0.0.0.0/0"
+  }
+}
+
+resource "openstack_compute_secgroup_v2" "secgroup_2" {
+  name                = "TerraformSSH"
+  description         = "Terraform SH for Port 22"
+
+  rule {
+    from_port         = 22
+    to_port           = 22
+    ip_protocol       = "tcp"
+    cidr              = "0.0.0.0/0" # only for demo purposes, tighten up in live scenario
+  }
+}
+
+# Create a port
+## SAN 1 network
+ resource "openstack_networking_port_v2" "port" {
+  for_each = var.instances
+
+  name           = each.key
+  network_id     = "${openstack_networking_network_v2.network[each.value[1]]}"
+  admin_state_up = "true"
+  security_group_ids  = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
+  
+  fixed_ip {
+    subnet_id         = "${openstack_networking_subnet_v2.subnets[each.value[1]].id}"
+    ip_address        = each.value[0]
+  }
+ }
+
+ resource "openstack_networking_port_v2" "port_1" {
+  name                = "port_1"
+  network_id          = "${openstack_networking_network_v2.network["SAN1"].id}"
+  admin_state_up      = "true"
+  security_group_ids  = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
+
+  fixed_ip {
+    subnet_id         = "${openstack_networking_subnet_v2.subnets["SAN1"].id}"
+    ip_address        = var.instances["VmServer"]
+  }
+}
+
+resource "openstack_networking_port_v2" "port_2" {
+  name                = "port_2"
+  network_id          = "${openstack_networking_network_v2.network["SAN1"].id}"
+  admin_state_up      = "true"
+  security_group_ids  = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
+
+  fixed_ip {
+    subnet_id         = "${openstack_networking_subnet_v2.subnets["SAN1"].id}"
+    ip_address        = var.instances["DbServer1"]
+  }
+}
+resource "openstack_networking_port_v2" "port_3" {
+  name                = "port_3"
+  network_id          = "${openstack_networking_network_v2.network["SAN1"].id}"
+  admin_state_up      = "true"
+  security_group_ids  = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
+
+  fixed_ip {
+    subnet_id         = "${openstack_networking_subnet_v2.subnets["SAN1"].id}"
+    ip_address        = var.instances["FileServer1"]
+  }
+}
+## SAN 2 network
+resource "openstack_networking_port_v2" "port_4" {
+  name                = "port_4"
+  network_id          = "${openstack_networking_network_v2.network["IT-admin"].id}"
+  admin_state_up      = "true"
+  security_group_ids  = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
+
+  fixed_ip {
+    subnet_id         = "${openstack_networking_subnet_v2.subnets["IT-admin"].id}"
+    ip_address        = var.instances["AnsibleMaster"]
+  }
+}
+resource "openstack_networking_port_v2" "port_5" {
+  name                = "port_5"
+  network_id          = "${openstack_networking_network_v2.network["SAN2"].id}"
+  admin_state_up      = "true"
+  security_group_ids  = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
+
+  fixed_ip {
+    subnet_id         = "${openstack_networking_subnet_v2.subnets["SAN2"].id}"
+    ip_address        = var.instances["DbServer2"]
+  }
+}
+resource "openstack_networking_port_v2" "port_6" {
+  name                = "port_6"
+  network_id          = "${openstack_networking_network_v2.network["SAN2"].id}"
+  admin_state_up      = "true"
+  security_group_ids  = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
+
+  fixed_ip {
+    subnet_id         = "${openstack_networking_subnet_v2.subnets["SAN2"].id}"
+    ip_address        = var.instances["FileServer2"]
+  }
+}
+## admin network
+resource "openstack_networking_port_v2" "NAS" {
+  name                = "port_7"
+  network_id          = "${openstack_networking_network_v2.network["IT-admin"].id}"
+  admin_state_up      = "true"
+  security_group_ids  = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
+
+  fixed_ip {
+    subnet_id         = "${openstack_networking_subnet_v2.subnets["IT-admin"].id}"
+    ip_address        = var.instances["NAS"]
+  }
+}
+## dmz network
+resource "openstack_networking_port_v2" "web" {
+  name                = "port_8"
+  network_id          = "${openstack_networking_network_v2.network["DMZ"].id}"
+  admin_state_up      = "true"
+  security_group_ids  = ["${openstack_compute_secgroup_v2.secgroup_1.id}"]
+
+  fixed_ip {
+    subnet_id         = "${openstack_networking_subnet_v2.subnets["DMZ"].id}"
+    ip_address        = var.instances["web"]
+  }
+}
+# Connect the subnet to the router
+resource "openstack_networking_router_interface_v2" "router_interface_1" {
+  router_id           = var.router_id
+  subnet_id           = "${openstack_networking_subnet_v2.subnets["DMZ"].id}"
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_2" {
+  router_id           = var.router_id
+  subnet_id           = "${openstack_networking_subnet_v2.subnets["Film/foto"].id}"
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_3" {
+  router_id           = var.router_id
+  subnet_id           = "${openstack_networking_subnet_v2.subnets["HR/ekonomi"].id}"
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_4" {
+  router_id           = var.router_id
+  subnet_id           = "${openstack_networking_subnet_v2.subnets["R&D"].id}"
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_5" {
+  router_id           = var.router_id
+  subnet_id           = "${openstack_networking_subnet_v2.subnets["IT-admin"].id}"
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_6" {
+  router_id           = var.router_id
+  subnet_id           = "${openstack_networking_subnet_v2.subnets["SAN1"].id}"
+}
+
+resource "openstack_networking_router_interface_v2" "router_interface_7" {
+  router_id           = var.router_id
+  subnet_id           = "${openstack_networking_subnet_v2.subnets["SAN2"].id}"
+}
+
+resource "openstack_compute_instance_v2" "server" {
+  for_each            = var.instances
+  name                = each.key
+  image_name          = var.image_name
+  flavor_name         = var.flavor_name
+  security_groups     = ["default","${openstack_compute_secgroup_v2.secgroup_1.name}","${openstack_compute_secgroup_v2.secgroup_2.name}"]
+  user_data           = var.cloudconfig_web
+  network {
+  port              = "${openstack_networking_port_v2.port[each.key].id}"
+}
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,20 @@
 ## VARIABLES
 # Make changes here
 
-variable "instances_name" {
-type      = list(string)
-default = [
-    "VmServer1",
-    "AnsibleMaster",
-    "DbServer1",
-    "DbServer2",
-    "FileServer1",
-    "FileServer2",
-    "NAS",
-    "web"
-  ]
+
+# template <name> = [<ip_address>, <network>]
+variable "instances" {
+type      = map(list(string))
+default = {
+    "VmServer" = ["192.168.6.50", "SAN1"],
+    "AnsibleMaster" = ["192.168.5.51", "IT-admin"],
+    "DbServer1" = ["192.168.6.51", "SAN1"],
+    "DbServer2" = ["192.168.7.50", "SAN2"],
+    "FileServer1" = ["192.168.6.52", "SAN1"],
+    "FileServer2" = ["192.168.7.51", "SAN2"],
+    "NAS" = ["192.168.5.50", "IT-admin"],
+    "web" = ["192.168.1.50", "DMZ"]
+  }
 }
 
 variable "networks" {
@@ -23,8 +25,8 @@ default = {
 "HR/ekonomi" = "192.168.3.0/24",
 "R&D" = "192.168.4.0/24",
 "IT-admin" = "192.168.5.0/24",
-"SAN 1" = "192.168.6.0/24",
-"SAN 2" = "192.168.7.0/24",
+"SAN1" = "192.168.6.0/24",
+"SAN2" = "192.168.7.0/24",
 }
 }
 
@@ -48,49 +50,9 @@ variable "dns_ip" {
   default = [ "8.8.8.8", "8.8.4.4" ]
 }
 
-variable "port_ip_vm1" {
-  type    = string
-  default = "192.168.6.50"
-}
-
-variable "port_ip_ansibleMaster" {
-  type    = string
-  default = "192.168.5.51"
-}
-
-variable "port_ip_db1" {
-  type    = string
-  default = "192.168.6.51"
-}
-
-variable "port_ip_db2" {
-  type    = string
-  default = "192.168.7.51"
-}
-
-variable "port_ip_fs1" {
-  type    = string
-  default = "192.168.6.52"
-}
-
-variable "port_ip_fs2" {
-  type    = string
-  default = "192.168.7.52"
-}
-
-variable "port_ip_nas" {
-  type    = string
-  default = "192.168.5.50"
-}
-
-variable "port_ip_web" {
-  type    = string
-  default = "192.168.1.50"
-}
-
 variable "router_id" {
   type    = string
-  default = "a44aee96-bc0c-4a6f-bf2c-e3007444428f"
+  default = "e3ee40b5-7707-4634-a725-144e81545c31"
 }
 
 variable "fip_pool" {
