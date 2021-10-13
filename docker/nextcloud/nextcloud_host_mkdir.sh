d95fd149c140f7ae009a51ba70a5340b88a920d7
Leo Bergström <Leo.Bergstrom@yh.nackademin.se>
2021-10-13 13:22:53 +0200

    clean up nextcloud_host_mkdir.sh

--- docker/nextcloud/nextcloud_host_mkdir.sh
+++ docker/nextcloud/nextcloud_host_mkdir.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #mkdir for nextcloud docker
-nextcloud_dir=($HOME/nextcloud \
-	$HOME/nextcloud/data \
+mkdir -p $HOME/nextcloud/data \
 	$HOME/nextcloud/data/html \
 	$HOME/nextcloud/data/config \
 	$HOME/nextcloud/data/apps \
 	$HOME/nextcloud/data/data \
 	$HOME/nextcloud/data/themes \
 	$HOME/nextcloud/data/mysql \
-	$HOME/nextcloud/data/collabora)
-
-for dir in "${nextcloud_dir[@]}"
-do 
-  mkdir $dir
-done
+	$HOME/nextcloud/data/collabora\
+	$HOME/jellyfin/data/config \
+	$HOME/jellyfin/data/cache \
+	$HOME/jellyfin/data/media \
+	$HOME/jellyfin/data/media2
