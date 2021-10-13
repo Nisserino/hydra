c528880434ef22ca94a8740aaa35b728f6528209
Ramus Nielsen <nielsen.rasmus@pm.me>
2021-10-13 14:39:08 +0000

    add docker play and docker scripts

---
+++ ansible/scripts/docker/nextcloud_host_mkdir.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#mkdir for nextcloud docker
+nextcloud_dir=(/home/nisse/nextcloud \
+	/home/nisse/nextcloud/data \
+	/home/nisse/nextcloud/data/html \
+	/home/nisse/nextcloud/data/config \
+	/home/nisse/nextcloud/data/apps \
+	/home/nisse/nextcloud/data/data \
+	/home/nisse/nextcloud/data/themes \
+	/home/nisse/nextcloud/data/mysql \
+	/home/nisse/nextcloud/data/collabora)
+
+for dir in "${nextcloud_dir[@]}"
+do 
+  mkdir $dir
+done
+
+mkdir -p /home/nisse/jellyfin/data/config \
+				/home/nisse/jellyfin/data/media \
+				/home/nisse/jellyfin/data/media2 \
+				/home/nisse/jellyfin/data/cache\
