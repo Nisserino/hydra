40fa630c7edc4e4d56b5d1b64b39f1e536ffd61d
Leo Bergström <ettsuddegum@protonmail.com>
2021-10-13 04:18:08 +0200

    added jellyfin

--- docker/nextcloud/nextcloud_init.sh
+++ docker/nextcloud/nextcloud_init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-alias nc-app-exec="docker exec --user www-data nextcloud-app php"
+ncAppExec="docker exec --user www-data nextcloud-app php"
 
 NEXTCLOUD_CMD=("occ app:install calendar" \
 "occ app:install apporder" \
 "occ app:install richdocuments" \
-"occ app:install spreed" )
+"occ app:install spreed")
 
 #echo ${NEXTCLOUD_CMD[*]}
 
 for CMD in "${NEXTCLOUD_CMD[@]}"
 do	
-   nc-app-exec $CMD
+   ${ncAppExec} ${CMD}
 done
 
 
