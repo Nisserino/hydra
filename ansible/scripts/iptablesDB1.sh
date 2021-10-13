9d2380d635fd9e4952f45e49536298d1d6d2d963
Nisse <nielsen.rasmus@pm.me>
2021-10-13 23:05:26 +0200

    remove old iptables

--- ansible/scripts/iptablesDB1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-iptables -A INPUT -s 192.168.0.0/16 -p tcp --dport 22 -j ACCEPT
-
-iptables -A INPUT -j DROP
--- ansible/scripts/iptablesWeb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-iptables -A INPUT -s 192.168.5.0/24 -p tcp --dport 22 -j ACCEPT
-
-iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-
-iptables -A INPUT -j DROP
