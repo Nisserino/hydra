1042e498df20ead2707bbfde40d0e758b604b973
Sebastian0-0 <sebbehanna@gmail.com>
2021-10-13 17:53:35 +0000

    minor fixes

--- ansible/scripts/iptablesVM.sh
+++ ansible/scripts/iptablesVM.sh
@@ -2,20 +2,20 @@
 
 iptables -A INPUT -s 192.168.0.0/16 -p tcp --dport 3306 -j ACCEPT
 
-iptables -A OUTPUT -s 192.168.0.0/16 -p tcp --match multiport --sport 3306 -j ACCEPT
+iptables -A OUTPUT -s 192.168.0.0/16 -p tcp --match --sport 3306 -j ACCEPT
 
 
 iptables -A INPUT -s 192.168.0.0/16 -p tcp --dport 8080 -j ACCEPT
 
-iptables -A OUTPUT -s 192.168.0.0/16 -p tcp --match multiport --sport 8080 -j ACCEPT
+iptables -A OUTPUT -s 192.168.0.0/16 -p tcp --sport 8080 -j ACCEPT
 
 
 iptables -A INPUT -s 192.168.0.0/16 -p tcp --dport 8096 -j ACCEPT
 
-iptables -A OUTPUT -s 192.168.0.0/16 -p tcp --match multiport --sport 8096 -j ACCEPT
+iptables -A OUTPUT -s 192.168.0.0/16 -p tcp --sport 8096 -j ACCEPT
 
 iptables -A INPUT -s 192.168.0.0/16 -p tcp --dport 9980 -j ACCEPT
 
-iptables -A OUTPUT -s 192.168.0.0/16 -p tcp --match multiport --sport 9980 -j ACCEPT
+iptables -A OUTPUT -s 192.168.0.0/16 -p tcp --sport 9980 -j ACCEPT
 
 
